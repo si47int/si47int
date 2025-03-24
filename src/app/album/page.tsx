dd881f44e350d8a05f4462898ec101b9736619df
Azdetic <agungindramawan@outlook.com>
2025-03-24 23:36:19 +0800

    Add Spotify Link

--- src/app/album/page.tsx
+++ src/app/album/page.tsx
@@ -13,6 +13,7 @@ interface SocialLinks {
   youtube?: string;
   email?: string;
   website?: string;
+  spotify?: string; // Added Spotify field
 }
 
 interface ClassMember {
@@ -42,6 +43,8 @@ export default function Album() {
         github: "https://github.com/Azdetic",
         email: "mailto:agungindramawan@outlook.com?subject=Hi%20Wira",
         website: "https://azdetic.github.io/Agwira",
+        spotify:
+          "https://open.spotify.com/user/c9f0dqmj29gofdem0ta0ur5gh?si=d33d68b4cf1d4cd6",
       },
     },
     {
@@ -57,6 +60,7 @@ export default function Album() {
         youtube: "#",
         email: "#",
         website: "#",
+        spotify: "#",
       },
     },
     {
@@ -72,6 +76,7 @@ export default function Album() {
         youtube: "#",
         website: "#",
         email: "mailto:rasyaakbarlptp@gmail.com?subject=Hi%20Rasya",
+        spotify: "#",
       },
     },
     // Add more members here with the same structure
@@ -228,6 +233,23 @@ export default function Album() {
                       viewBox="0 0 24 24"
                     >
                       <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L8 14v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z" />
+                    </svg>
+                  </Link>
+                )}
+
+                {/* Spotify - New icon for Spotify */}
+                {member.socialLinks.spotify && (
+                  <Link
+                    href={member.socialLinks.spotify}
+                    className="text-white/80 hover:text-white transition"
+                    aria-label="Spotify"
+                  >
+                    <svg
+                      className="w-6 h-6"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z" />
                     </svg>
                   </Link>
                 )}
