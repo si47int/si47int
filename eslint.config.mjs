4dd4ae264f104ea09cdd4c11668821d5609b255d
Azdetic <agungindramawan@outlook.com>
2025-04-28 10:42:57 +0700

    Fix 3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,10 +11,10 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next'],
+    extends: ["next"],
     rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
     },
   }),
 ];
